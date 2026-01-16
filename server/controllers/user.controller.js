695fd2663cf3d2edf102f5a352605cdbc56e32c5
Samradh Sahni <samradhsahni@gmail.com>
2026-01-16 22:25:06 +0530

    Add files via upload

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
